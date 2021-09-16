3305a4a43b7385088245d746c0251a0fc3847006
saldin <saldin393@gmail.com>
2021-09-16 09:41:44 +0200

    nov zacetek2

--- index.php
+++ index.php
@@ -1,11 +1,134 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <title>AddMe Homepage</title>
-  </head>
-  <body>
+<?php include 'header.php'; ?>
+<?php include 'style.css'; ?>
+section class="main">
+    <div class="wrapper">
+        <div class="left-col">
+            // status wrappers
 
-  </body>
-</html>
+            <div class="post">
+                <div class="info">
+                    <div class="user">
+                        <div class="profile-pic"><img src="img/cover 1.png" alt=""></div>
+                        <p class="username">modern_web_channel</p>
+                    </div>
+                    <img src="img/option.PNG" class="options" alt="">
+                </div>
+                <img src="img/cover 1.png" class="post-image" alt="">
+                <div class="post-content">
+                    <div class="reaction-wrapper">
+                        <img src="img/like.PNG" class="icon" alt="">
+                        <img src="img/comment.PNG" class="icon" alt="">
+                        <img src="img/send.PNG" class="icon" alt="">
+                        <img src="img/save.PNG" class="save icon" alt="">
+                    </div>
+                    <p class="likes">1,012 likes</p>
+                    <p class="description"><span>username </span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis placeat, molestiae impedit aut provident eum quo natus molestias?</p>
+                    <p class="post-time">2 minutes ago</p>
+                </div>
+                <div class="comment-wrapper">
+                    <img src="img/smile.PNG" class="icon" alt="">
+                    <input type="text" class="comment-box" placeholder="Add a comment">
+                    <button class="comment-btn">post</button>
+                </div>
+            </div>
+            <div class="post">
+                <div class="info">
+                    <div class="user">
+                        <div class="profile-pic"><img src="img/cover 2.png" alt=""></div>
+                        <p class="username">modern_web_channel</p>
+                    </div>
+                    <img src="img/option.PNG" class="options" alt="">
+                </div>
+                <img src="img/cover 2.png" class="post-image" alt="">
+                <div class="post-content">
+                    <div class="reaction-wrapper">
+                        <img src="img/like.PNG" class="icon" alt="">
+                        <img src="img/comment.PNG" class="icon" alt="">
+                        <img src="img/send.PNG" class="icon" alt="">
+                        <img src="img/save.PNG" class="save icon" alt="">
+                    </div>
+                    <p class="likes">1,012 likes</p>
+                    <p class="description"><span>username </span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis placeat, molestiae impedit aut provident eum quo natus molestias?</p>
+                    <p class="post-time">2 minutes ago</p>
+                </div>
+                <div class="comment-wrapper">
+                    <img src="img/smile.PNG" class="icon" alt="">
+                    <input type="text" class="comment-box" placeholder="Add a comment">
+                    <button class="comment-btn">post</button>
+                </div>
+            </div>
+            // +5 more post elements
+        </div>
+    </div>
+</section>
+<section class="main">
+    <div class="wrapper">
+        // left col element
+        <div class="right-col">
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/profile-pic.png" alt="">
+                </div>
+                <div>
+                    <p class="username">modern_web_channel</p>
+                    <p class="sub-text">kunaal kumar</p>
+                </div>
+                <button class="action-btn">switch</button>
+            </div>
+            <p class="suggestion-text">Suggestions for you</p>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 9.png" alt="">
+                </div>
+                <div>
+                    <p class="username">modern_web_channel</p>
+                    <p class="sub-text">followed bu user</p>
+                </div>
+                <button class="action-btn">follow</button>
+            </div>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 10.png" alt="">
+                </div>
+                <div>
+                    <p class="username">modern_web_channel</p>
+                    <p class="sub-text">followed bu user</p>
+                </div>
+                <button class="action-btn">follow</button>
+            </div>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 11.png" alt="">
+                </div>
+                <div>
+                    <p class="username">modern_web_channel</p>
+                    <p class="sub-text">followed bu user</p>
+                </div>
+                <button class="action-btn">follow</button>
+            </div>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 12.png" alt="">
+                </div>
+                <div>
+                    <p class="username">modern_web_channel</p>
+                    <p class="sub-text">followed bu user</p>
+                </div>
+                <button class="action-btn">follow</button>
+            </div>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 13.png" alt="">
+                </div>
+                <div>
+                    <p class="username">modern_web_channel</p>
+                    <p class="sub-text">followed bu user</p>
+                </div>
+                <button class="action-btn">follow</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include 'footer.php'; ?>
